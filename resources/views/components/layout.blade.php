861d1b5a7d8e919f8501172cc0e64707eeba86d0
Igiz <igizstarkle@gmail.com>
2025-02-04 15:03:27 +0300

    vite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    @vite(['resources/js/app.js'])
 </head>
 <body>
+
+    <div>
+        <nav>
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/blog.png') }}" alt="" srcset="">
+                </a>
+            </div>
+
+            <div>
+                link
+            </div>
+            <div>
+                Post a Job
+            </div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
     
 </body>
 </html>
